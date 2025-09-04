1b66683c6722ddf02c0c60a37d22e758ca2ae287
Timrocketsman <timrocketsman@gmail.com>
2025-09-04 23:10:21 +0300

    добавлен OPENAI_API_KEY

--- app.py
+++ app.py
@@ -6,6 +6,7 @@ app = Flask(__name__)
 
 TELEGRAM_TOKEN = "8336549556:AAFcMSnakNYWbUGgPRMNV4E2k91GIfyupjQ"
 CHAT_ID = "3089784234"
+OPENAI_API_KEY = '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'
 
 @app.route("/webhook", methods=["POST"])
 def webhook():
