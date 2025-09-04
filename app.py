f580d16d4ac0615db01624a1a119ec70eae48be5
Timrocketsman <timrocketsman@gmail.com>
2025-09-04 23:11:20 +0300

    используются только переменные окружения

--- app.py
+++ app.py
@@ -4,9 +4,9 @@ from flask import Flask, request
 
 app = Flask(__name__)
 
-TELEGRAM_TOKEN = "8336549556:AAFcMSnakNYWbUGgPRMNV4E2k91GIfyupjQ"
-CHAT_ID = "3089784234"
-OPENAI_API_KEY = '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'
+TELEGRAM_TOKEN = os.environ.get('TELEGRAM_TOKEN')
+CHAT_ID = os.environ.get('CHAT_ID')
+OPENAI_API_KEY = os.environ.get('OPENAI_API_KEY')
 
 @app.route("/webhook", methods=["POST"])
 def webhook():
